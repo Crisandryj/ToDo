dfb6fda2e37ff3cc8143a6eeeaa8fd9608dbe752
crisandryj <crisandryj@gmail.com>
2023-08-02 12:28:57 -0400

    Add notes to form addlistener

--- src/index.js
+++ src/index.js
@@ -20,16 +20,18 @@ let defaultProject = new Project("Default")
 //select list form
 const itemListForm = document.getElementById("itemList")
 
-// collect input from form and create item object
+// collect input from form
 itemListForm.addEventListener('submit', (event) => {
   let title = itemListForm['title'].value
   let description = itemListForm['description'].value
   let dueDate = itemListForm['dueDate'].value
   let priority = itemListForm['priority'].value
+  //create item
   let item = new ToDoItem(title,description,dueDate,priority)
+  //add to defualt project
   defaultProject.items.push(item)
   console.log(defaultProject.items)
-
+  //clear form
   itemListForm.reset()
   // stop form submission
   event.preventDefault();
