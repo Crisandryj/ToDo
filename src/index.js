b5e37ee2abb6bb5d4174e1ecbca4a646f70f3428
crisandryj <crisandryj@gmail.com>
2023-08-02 11:18:03 -0400

    Feat: Add calander to form

--- src/index.js
+++ src/index.js
@@ -20,14 +20,14 @@ let defaultProject = new Project("Default")
 //select list form
 const itemListForm = document.getElementById("itemList")
 
+// collect inputted item in form
 itemListForm.addEventListener('submit', (event) => {
-  itemListForm['title'].value
-  itemListForm['description'].value
-  itemListForm['dueDate'].value
-  itemListForm['priority'].value
+  let title = itemListForm['title'].value
+  let description = itemListForm['description'].value
+  let dueDate = itemListForm['dueDate'].value
+  let priority = itemListForm['priority'].value
   // stop form submission
   event.preventDefault();
- 
 });
 
 console.log(itemListForm['name'])
