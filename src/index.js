a2f1d5ae8b0731ff9a899869e90bf14fc1487e70
crisandryj <crisandryj@gmail.com>
2023-08-07 10:33:09 -0400

    lint

--- src/index.js
+++ src/index.js
@@ -1,65 +1,59 @@
-import {ToDoItem} from "./todoitem.js";
-import {Project} from "./project.js";
+import { ToDoItem } from './todoitem.js';
+import { Project } from './project.js';
 
+// create default project to add items
+const defaultProject = new Project('Default');
 
-//create default project to add items
-let defaultProject = new Project("Default")
+// select container
+const content = document.querySelector('#content');
+// select projects button
+const btn = document.querySelector('#projects');
 
-//select container
-const content = document.querySelector('#content')
-//select projects button
-const btn = document.querySelector('#projects')
-
-//add heading to home page
-function homeDom(){
-  const heading = document.createElement('h1')
-  content.appendChild(heading)
+// add heading to home page
+function homeDom() {
+  const heading = document.createElement('h1');
+  content.appendChild(heading);
 }
-//display home heading
-homeDom()
+// display home heading
+homeDom();
 
-//select list
-let list = document.querySelector('.items')
+// select list
+const list = document.querySelector('.items');
 
-//Add item to list
-function updateList (i){
-  let item = document.createElement('input')
-  item.setAttribute('type','checkbox')
-  item.setAttribute('id',i.title)
-  let itemName = document.createElement('label')
-  itemName.setAttribute('for', "done")
-  itemName.textContent = i.title
-  console.log(item)
-  list.appendChild(itemName)
-  itemName.appendChild(item)
+// Add item to list
+function updateList(i) {
+  const item = document.createElement('input');
+  item.setAttribute('type', 'checkbox');
+  item.setAttribute('id', i.title);
+  const itemName = document.createElement('label');
+  itemName.setAttribute('for', 'done');
+  itemName.textContent = i.title;
+  list.appendChild(itemName);
+  itemName.appendChild(item);
 }
 
-btn.addEventListener('click', updateList)
+btn.addEventListener('click', updateList);
 
+const checkbox = document.querySelector('input[name=checkbox]');
 
-let checkbox = document.querySelector("input[name=checkbox]");
+checkbox.addEventListener('change', alert('hi'));
 
-checkbox.addEventListener('change',alert('hi'))
-
-//select list form
-const itemListForm = document.getElementById("itemList")
+// select list form
+const itemListForm = document.getElementById('itemList');
 
 // collect input from form
 itemListForm.addEventListener('submit', (event) => {
-  let title = itemListForm['title'].value
-  let description = itemListForm['description'].value
-  let dueDate = itemListForm['dueDate'].value
-  let priority = itemListForm['priority'].value
-  //create item
-  let item = new ToDoItem(title,description,dueDate,priority)
-  //add to defualt project
-  defaultProject.items.push(item)
-  updateList(item)
-  //clear form
-  itemListForm.reset()
+  const title = itemListForm.title.value;
+  const description = itemListForm.description.value;
+  const dueDate = itemListForm.dueDate.value;
+  const priority = itemListForm.priority.value;
+  // create item
+  const item = new ToDoItem(title, description, dueDate, priority);
+  // add to defualt project
+  defaultProject.items.push(item);
+  updateList(item);
+  // clear form
+  itemListForm.reset();
   // stop form submission
   event.preventDefault();
 });
-
-
-
