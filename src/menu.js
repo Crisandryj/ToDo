f270f90a8a907c26911bfcf7c17ed1d29797e397
crisandryj <crisandryj@gmail.com>
2023-08-14 19:38:29 -0400

    Remove line

--- src/menu.js
+++ src/menu.js
@@ -8,7 +8,6 @@ document.addEventListener("click", (e) => {
     currentDropdown = e.target.closest('[data-dropdown]')
     currentDropdown.classList.toggle('active')
   }
-
   document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
     if (dropdown === currentDropdown) return
     dropdown.classList.remove('active')
