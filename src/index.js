c433d3837c3b455ca731b828367cec351eb2c5e1
crisandryj <crisandryj@gmail.com>
2023-08-18 07:09:36 -0400

    Feat: list project names on site

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import {drop} from './menu'
 import {Project} from './project'
 
 const projectForm = document.getElementById('projectform')
-const projectInstances = [];
 const newProjectBtn = document.querySelector('#newproject')
+const content = document.querySelector('.content')
+
+const projectInstances = [];
+
+function addProjectToList (project){
+  const list = document.createElement('ul')
+  content.appendChild(list)
+  const listItem = document.createElement('li')
+  content.appendChild(listItem)
+  listItem.textContent = project
+}
 
 // Create new project
 projectForm.addEventListener('submit',(e)=>{
@@ -11,15 +21,18 @@ projectForm.addEventListener('submit',(e)=>{
   const project = projectForm.elements.name
   const newProject = new Project(project.value)
   projectInstances.push(newProject)
+  addProjectToList(newProject.name)
   projectForm.reset()
 } )
-
-
 
 // Hide/Show form when button clicked
 newProjectBtn.addEventListener('click', () =>{
   projectForm.classList.toggle('hide')
 })
 
-console.log(projectInstances)
+// List projects
+
+
+
+
 
